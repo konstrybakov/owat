f6280590e7da5c65099ac7ab3dbc2eac8eb478c5
Konstantin Rybakov <i@krybakov.com>
2024-06-12 09:12:31 +0300

    rka-34: add logging

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,5 +1,31 @@
-import { pino } from 'pino'
+import { type TransportTargetOptions, pino } from 'pino'
 
-export const logger = pino({
-  level: process.env.LOG_LEVEL || 'info',
+const targets: TransportTargetOptions[] = [
+  {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+    },
+  },
+]
+
+if (process.env.NODE_ENV === 'production') {
+  targets.push({
+    target: '@axiomhq/pino',
+    options: {
+      dataset: process.env.AXIOM_DATASET,
+      token: process.env.AXIOM_API_TOKEN,
+    },
+  })
+}
+
+const transport = pino.transport({
+  targets,
 })
+
+export const logger = pino(
+  {
+    level: process.env.LOG_LEVEL || 'info',
+  },
+  transport,
+)
