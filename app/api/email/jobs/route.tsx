895252041b956e7801e257a06aab25db7900c450
Konstantin Rybakov <i@krybakov.com>
2024-07-25 08:07:57 +0300

    rka-27: change route verb (#13)

--- app/api/email/jobs/route.tsx
+++ app/api/email/jobs/route.tsx
@@ -7,7 +7,7 @@ import { Resend } from 'resend'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-export const POST = async () => {
+export const GET = async () => {
   try {
     const headerList = headers()
     const auth = headerList.get('Authorization')
