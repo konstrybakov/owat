180016df9c5942a5c582fbfed81fe9095763b186
Konstantin Rybakov <i@krybakov.com>
2024-07-28 13:35:44 +0300

    rka-48, rka-51: fix no results in list view

--- app/components/list/job-list.tsx
+++ app/components/list/job-list.tsx
@@ -2,7 +2,8 @@ import { type GetJobsFilter, queryGetJobs } from '@/lib/db/queries'
 import { JobCard } from './job-card'
 
 import type { PageSearchParams } from '@/app/types'
-import { Flex, Reset } from '@radix-ui/themes'
+import { ValueIcon } from '@radix-ui/react-icons'
+import { Callout, Flex, Reset } from '@radix-ui/themes'
 import { FilterPanel } from './filter-panel'
 import { Pagination } from './pagination'
 
@@ -17,22 +18,35 @@ export const JobList = async ({ searchParams }: JobListProps) => {
     searchParams.page ? Number(searchParams.page) : 1,
   )
 
+  const hasJobs = result.data.length > 0
+
   return (
     <Flex direction="column" gap="3">
       <FilterPanel />
       <Flex asChild direction="column" gap="3">
-        <Reset>
-          {/* biome-ignore lint/a11y/noRedundantRoles: safari 👀 */}
-          <ul role="list">
-            {result.data.map(job => (
-              <li key={job.id}>
-                <JobCard job={job} />
-              </li>
-            ))}
-          </ul>
-        </Reset>
+        {hasJobs ? (
+          <Reset>
+            {/* biome-ignore lint/a11y/noRedundantRoles: safari 👀 */}
+            <ul role="list">
+              {result.data.map(job => (
+                <li key={job.id}>
+                  <JobCard job={job} />
+                </li>
+              ))}
+            </ul>
+          </Reset>
+        ) : (
+          <Callout.Root size="3">
+            <Callout.Icon>
+              <ValueIcon />
+            </Callout.Icon>
+            <Callout.Text>
+              No jobs found. Try changing the filters.
+            </Callout.Text>
+          </Callout.Root>
+        )}
       </Flex>
-      <Pagination total={result.total} />
+      {hasJobs ? <Pagination total={result.total} /> : null}
     </Flex>
   )
 }
